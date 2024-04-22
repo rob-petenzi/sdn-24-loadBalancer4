5c1b2c59810f77feb0424a308911b7a4e848539f
Roberto Petenzi <98556067+rob-petenzi@users.noreply.github.com>
2024-04-23 01:36:17 +0200

    Update chat4.py

--- chat4.py
+++ chat4.py
@@ -3,7 +3,7 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet, ethernet
+from ryu.lib.packet import packet, ethernet, ether_types
 
 class SimpleLoadBalancingController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
