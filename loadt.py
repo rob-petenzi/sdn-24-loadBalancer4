e91b107d2733eb1abd1b4b7d426a5f5133b6deb1
EMoscatello <65389302+EMoscatello@users.noreply.github.com>
2024-04-23 16:18:27 +0200

    Update loadt.py

--- loadt.py
+++ loadt.py
@@ -11,6 +11,7 @@ class PsrSwitch(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(PsrSwitch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.switch_neighbors = {}  # To store neighbors of each switch
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -20,6 +21,13 @@ class PsrSwitch(app_manager.RyuApp):
 
         # Install default flow entries for unknown traffic
         self.install_default_flow(datapath)
+
+        # Get switch's datapath id (DPID)
+        dpid = datapath.id
+
+        # Determine neighbors based on torus topology
+        neighbors = self.get_switch_neighbors(dpid)
+        self.switch_neighbors[dpid] = neighbors
 
     def install_default_flow(self, datapath):
         ofproto = datapath.ofproto
@@ -46,10 +54,10 @@ class PsrSwitch(app_manager.RyuApp):
         )
         datapath.send_msg(mod)
 
-        # Table 1: Flood packets for unknown destination addresses
+        # Table 1: Forward packets based on learned MAC to port mapping
         match = parser.OFPMatch()
         actions = [
-            parser.OFPActionOutput(ofproto.OFPP_FLOOD)
+            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, 128)
         ]
         inst = [
             parser.OFPInstructionActions(
@@ -65,6 +73,15 @@ class PsrSwitch(app_manager.RyuApp):
             instructions=inst
         )
         datapath.send_msg(mod)
+
+    def get_switch_neighbors(self, dpid):
+        # Implement logic to determine neighbors based on torus topology
+        # For a 3x3 torus, switches are connected in a grid-like pattern
+        # where each switch has 4 neighbors (north, south, east, west)
+        # Consider wrapping around at the edges to form a torus topology
+        # For example, switch 1's neighbors are switches 2, 4, 8, and 6
+        # Return a list of neighboring switch DPIDs
+        pass
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -92,6 +109,8 @@ class PsrSwitch(app_manager.RyuApp):
         if dst in self.mac_to_port:
             out_port = self.mac_to_port[dst]
         else:
+            # If destination MAC address is unknown, flood packet to neighbors
+            # Implement logic to forward packet to neighboring switches
             out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
